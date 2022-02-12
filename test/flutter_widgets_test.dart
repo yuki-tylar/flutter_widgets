4cd6e164b10986d0cd0a2a4d116036f3652adfe2
takayuki <takayuki.hiraishi@gmail.com>
2022-02-11 22:32:03 -0800

    small change

--- test/flutter_widgets_test.dart
+++ test/flutter_widgets_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_widgets/text_block.dart';
+import 'package:widgets/text_block.dart';
 
 void main() {
   testWidgets(
